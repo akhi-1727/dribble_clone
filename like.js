023ea54a326bb592a7ddc6e0d80d4ac6a60d1e4a
akhi-1727 <140237704+akhi-1727@users.noreply.github.com>
2023-11-19 10:46:49 +0530

    Add files via upload

---
+++ like.js
@@ -0,0 +1,109 @@
+var btnvar1=document.getElementById("btn1");
+function Toggle1(){
+    if(btnvar1.style.color=="red"){
+        btnvar1.style.color="grey";
+        }
+    else{
+        btnvar1.style.color="red";
+        }
+    }
+    var btnvar2=document.getElementById("btn2");
+    function Toggle2(){
+        if(btnvar2.style.color=="red"){
+            btnvar2.style.color="grey";
+            }
+        else{
+            btnvar2.style.color="red";
+            }
+        }
+        var btnvar3=document.getElementById("btn3");
+        function Toggle3(){
+            if(btnvar3.style.color=="red"){
+                btnvar3.style.color="grey";
+                }
+            else{
+                btnvar3.style.color="red";
+                }
+            }
+            var btnvar4=document.getElementById("btn4");
+            function Toggle4(){
+                if(btnvar4.style.color=="red"){
+                    btnvar4.style.color="grey";
+                    }
+                else{
+                    btnvar4.style.color="red";
+                    }
+                }
+                var btnvar5=document.getElementById("btn5");
+                function Toggle5(){
+                    if(btnvar5.style.color=="red"){
+                        btnvar5.style.color="grey";
+                        }
+                    else{
+                        btnvar5.style.color="red";
+                        }
+                    }
+                    var btnvar6=document.getElementById("btn6");
+                    function Toggle6(){
+                        if(btnvar6.style.color=="red"){
+                            btnvar6.style.color="grey";
+                            }
+                        else{
+                            btnvar6.style.color="red";
+                            }
+                        }
+                        var btnvar7=document.getElementById("btn7");
+                        function Toggle7(){
+                            if(btnvar7.style.color=="red"){
+                                btnvar7.style.color="grey";
+                                }
+                            else{
+                                btnvar7.style.color="red";
+                                }
+                            }
+                            var btnvar8=document.getElementById("btn8");
+                            function Toggle8(){
+                                if(btnvar8.style.color=="red"){
+                                    btnvar8.style.color="grey";
+                                    }
+                                else{
+                                    btnvar8.style.color="red";
+                                    }
+                                }
+                                var btnvar9=document.getElementById("btn9");
+                                function Toggle9(){
+                                    if(btnvar9.style.color=="red"){
+                                        btnvar9.style.color="grey";
+                                        }
+                                    else{
+                                        btnvar9.style.color="red";
+                                        }
+                                    }
+                                    var btnvar10=document.getElementById("btn10");
+                                    function Toggle10(){
+                                        if(btnvar10.style.color=="red"){
+                                            btnvar10.style.color="grey";
+                                            }
+                                        else{
+                                            btnvar10.style.color="red";
+                                            }
+                                        }
+                                        var btnvar11=document.getElementById("btn11");
+                                        function Toggle11(){
+                                            if(btnvar11.style.color=="red"){
+                                                btnvar11.style.color="grey";
+                                                }
+                                            else{
+                                                btnvar11.style.color="red";
+                                                }
+                                            }
+                                            var btnvar12=document.getElementById("btn12");
+                                            function Toggle12(){
+                                                if(btnvar12.style.color=="red"){
+                                                    btnvar12.style.color="grey";
+                                                    }
+                                                else{
+                                                    btnvar12.style.color="red";
+                                                    }
+                                                }
+    
